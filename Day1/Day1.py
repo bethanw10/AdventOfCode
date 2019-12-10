66c0b51d16b94b75d6a79aa72953f4e7bcd4d67e
bethan.williams <bethan.williams@protolabs.co.uk>
2019-12-10 10:46:19 +0000

    Add part 2 for Day 1

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -5,7 +5,7 @@ def calculate_fuel(num):
     return math.trunc(num / 3) - 2
 
 
-if __name__ == "__main__":
+def part_1():
     file = open("input.txt", "r")
     total = 0
 
@@ -13,3 +13,27 @@ if __name__ == "__main__":
         total += calculate_fuel(int(line))
 
     print(total)
+
+
+def calculate_fuel_part_2(num, total):
+    fuel_cost = math.trunc(num / 3) - 2
+
+    if fuel_cost >= 1:
+        total += fuel_cost
+        return calculate_fuel_part_2(fuel_cost, total)
+
+    return total
+
+
+def part_2():
+    file = open("input.txt", "r")
+    total = 0
+
+    for line in file:
+        total += calculate_fuel_part_2(int(line), 0)
+
+    print(total)
+
+
+if __name__ == "__main__":
+    part_2()
