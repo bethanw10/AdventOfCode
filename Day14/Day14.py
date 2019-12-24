617746a2724aabb44dc54ff5c0f1fdb6b0b2c6c1
Bethan <bethanw10@gmail.com>
2019-12-24 16:32:16 +0000

    Finish day 14

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Ingredient:
     def __init__(self, name, amount):
         self.name = name
@@ -39,40 +40,76 @@ def load_recipes(content):
     return recipes
 
 
-def get_recipe(recipes, name, amount_needed, leftovers):
-    total = 0
-    recipe = recipes[name]
+def get_ore_needed(recipes, name, amount):
+    needed = {name: amount}
+    leftovers = {}
 
-    for ingredient in recipe.ingredients:
-        if ingredient.name in recipes:  # Not ORE
-            cost = get_recipe(recipes, ingredient.name, ingredient.amount, leftovers)
-            total += cost
+    while list(needed.keys()) != ['ORE']:
+        for target_name, target_needed in list(needed.items()):
+            if target_name == 'ORE':
+                continue
+
+            if target_name in leftovers:
+                if leftovers[target_name] > needed[target_name]:
+                    leftovers[target_name] -= needed[target_name]
+                    needed[target_name] = 0
+                else:
+                    needed[target_name] -= leftovers[target_name]
+                    leftovers[target_name] = 0
+            else:
+                leftovers[target_name] = 0
+
+            amount_needed = needed[target_name]
+            output = recipes[target_name].output
+
+            num_repeats = math.ceil(amount_needed / output)
+            leftovers[target_name] += (output * num_repeats) - amount_needed
+
+            for ingredient in recipes[target_name].ingredients:
+                if ingredient.name not in needed:
+                    needed[ingredient.name] = 0
+
+                needed[ingredient.name] += (ingredient.amount * num_repeats)
+
+            del needed[target_name]
+
+    return needed['ORE']
+
+
+def part_2(recipes):
+    lowest = 443537  # Makes 1
+    highest = 5_000_000_000
+    mid = 0
+
+    target = 1_000_000_000_000
+    found = False
+
+    while not found:
+        mid = (lowest + highest) // 2
+        output = get_ore_needed(recipes, 'FUEL', mid)
+
+        if lowest == highest + 1:
+            return mid
+
+        if output == target:
+            found = True
+        elif target < output:
+            highest = mid - 1
         else:
-            total += ingredient.amount
+            lowest = mid + 1
 
-    if name in leftovers:
-        if leftovers[name] > amount_needed:
-            leftovers[name] -= amount_needed
-            return 0
-        else:
-            amount_needed -= leftovers[name]
-            leftovers[name] = 0
-    else:
-        leftovers[name] = 0
-
-    quantity_needed = math.ceil(amount_needed / recipe.output)
-    leftovers[name] += (recipe.output * quantity_needed) - amount_needed
-
-    return total * quantity_needed
+    return mid
 
 
 def main():
-    f = open("input-test.txt", "r")
+    f = open("input.txt", "r")
     content = f.read()
 
     recipes = load_recipes(content)
 
-    print(get_recipe(recipes, 'FUEL', 1, {}))
+    print(get_ore_needed(recipes, 'FUEL', 1))
+
+    print(part_2(recipes))
 
 
 if __name__ == "__main__":
