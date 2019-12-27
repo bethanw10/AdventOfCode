966f3a3121a31324168e57ca5ef326ea2fe65156
Bethan <bethanw10@gmail.com>
2019-12-27 23:35:51 +0000

    Finish Day 11

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,103 +1,286 @@
-from enum import Enum
+# 1 - Add
+# 2 - Multiply
+# 3 - Input
+# 4 - Output
+# 5 - Jump if true
+# 6 - Jump if false
+# 7 - Less than
+# 8 - Equal
+# 9 - Change relative base
 
-# Op code 1 - add number
-# Op code 2 - multiply numbers
-# Op code 99 - halt
+DEBUG = False
 
 
-class ParameterMode(Enum):
+class IntCodeComputer:
     POSITION = '0'
     IMMEDIATE = '1'
+    RELATIVE = '2'
+
+    def __init__(self, content):
+        self.content = content
+        self.content.extend([0 for _ in range(1000)])
+
+        self.pointer = 0
+        self.relative_base = 0
+
+    def run(self, input_func=input, output_func=print):
+        while self.pointer <= len(self.content) - 1:
+            instruction = self.content[self.pointer]
+            op_code = int(instruction[-2:])
+            self.pointer += 1
+
+            if op_code == 99:
+                return
+
+            if 1 <= op_code <= 2:  # Arithmetic
+                parameter_modes = instruction[:-2].rjust(3, '0')
+
+                input_1, self.pointer = self.get_value(parameter_modes[-1])
+                input_2, self.pointer = self.get_value(parameter_modes[-2])
+
+                if op_code == 1:
+                    self.pointer = self.set_value(str(input_1 + input_2), parameter_modes[-3])
+                elif op_code == 2:
+                    self.pointer = self.set_value(str(input_1 * input_2), parameter_modes[-3])
+
+            elif op_code == 3:  # Input
+                parameter_modes = instruction[:-2].rjust(1, '0')
+                self.pointer = self.set_value(input_func(), parameter_modes[-1])
+
+            elif op_code == 4:  # Output
+                parameter_modes = instruction[:-2].rjust(1, '0')
+                value, self.pointer = self.get_value(parameter_modes[-1])
+                output_func(value)
+
+            elif op_code == 5:
+                parameter_modes = instruction[:-2].rjust(2, '0')
+
+                jump_if, self.pointer = self.get_value(parameter_modes[-1])
+
+                if jump_if != 0:
+                    self.pointer, _ = self.get_value(parameter_modes[-2])
+                else:
+                    self.pointer += 1
+
+            elif op_code == 6:
+                parameter_modes = instruction[:-2].rjust(2, '0')
+
+                jump_if, self.pointer = self.get_value(parameter_modes[-1])
+
+                if jump_if == 0:
+                    self.pointer, _ = self.get_value(parameter_modes[-2])
+                else:
+                    self.pointer += 1
+
+            elif 7 <= op_code <= 8:  # Comparative
+                parameter_modes = instruction[:-2].rjust(3, '0')
+
+                input_1, self.pointer = self.get_value(parameter_modes[-1])
+                input_2, self.pointer = self.get_value(parameter_modes[-2])
+
+                if op_code == 7:
+                    self.pointer = self.set_value(1 if input_1 < input_2 else 0, parameter_modes[-3])
+                elif op_code == 8:
+                    self.pointer = self.set_value(1 if input_1 == input_2 else 0, parameter_modes[-3])
+
+            elif op_code <= 9:
+                parameter_modes = instruction[:-2].rjust(1, '0')
+
+                value, self.pointer = self.get_value(parameter_modes[-1])
+                self.relative_base += value
+
+            if DEBUG:
+                print()
+                print(f"pointer {self.pointer} | ", f"instruction {instruction} |",
+                      f"next value {self.content[self.pointer]} |", self.content)
+                print()
+
+    def run_until_output(self, input_func=input):
+        while self.pointer <= len(self.content) - 1:
+            instruction = self.content[self.pointer]
+            op_code = int(instruction[-2:])
+            self.pointer += 1
+
+            if op_code == 99:
+                return
+
+            if 1 <= op_code <= 2:  # Arithmetic
+                parameter_modes = instruction[:-2].rjust(3, '0')
+
+                input_1, self.pointer = self.get_value(parameter_modes[-1])
+                input_2, self.pointer = self.get_value(parameter_modes[-2])
+
+                if op_code == 1:
+                    self.pointer = self.set_value(str(input_1 + input_2), parameter_modes[-3])
+                elif op_code == 2:
+                    self.pointer = self.set_value(str(input_1 * input_2), parameter_modes[-3])
+
+            elif op_code == 3:  # Input
+                parameter_modes = instruction[:-2].rjust(1, '0')
+                self.pointer = self.set_value(input_func(), parameter_modes[-1])
+
+            elif op_code == 4:  # Output
+                parameter_modes = instruction[:-2].rjust(1, '0')
+                value, self.pointer = self.get_value(parameter_modes[-1])
+                return value
+
+            elif op_code == 5:
+                parameter_modes = instruction[:-2].rjust(2, '0')
+
+                jump_if, self.pointer = self.get_value(parameter_modes[-1])
+
+                if jump_if != 0:
+                    self.pointer, _ = self.get_value(parameter_modes[-2])
+                else:
+                    self.pointer += 1
+
+            elif op_code == 6:
+                parameter_modes = instruction[:-2].rjust(2, '0')
+
+                jump_if, self.pointer = self.get_value(parameter_modes[-1])
+
+                if jump_if == 0:
+                    self.pointer, _ = self.get_value(parameter_modes[-2])
+                else:
+                    self.pointer += 1
+
+            elif 7 <= op_code <= 8:  # Comparative
+                parameter_modes = instruction[:-2].rjust(3, '0')
+
+                input_1, self.pointer = self.get_value(parameter_modes[-1])
+                input_2, self.pointer = self.get_value(parameter_modes[-2])
+
+                if op_code == 7:
+                    self.pointer = self.set_value(1 if input_1 < input_2 else 0, parameter_modes[-3])
+                elif op_code == 8:
+                    self.pointer = self.set_value(1 if input_1 == input_2 else 0, parameter_modes[-3])
+
+            elif op_code <= 9:
+                parameter_modes = instruction[:-2].rjust(1, '0')
+
+                value, self.pointer = self.get_value(parameter_modes[-1])
+                self.relative_base += value
+
+            if DEBUG:
+                print()
+                print(f"pointer {self.pointer} | ", f"instruction {instruction} |",
+                      f"next value {self.content[self.pointer]} |", self.content)
+                print()
+
+    def get_value(self, mode=POSITION):
+        if mode == self.POSITION:
+            address = int(self.content[self.pointer])
+            value = int(self.content[address])
+
+        elif mode == self.IMMEDIATE:
+            value = int(self.content[self.pointer])
+
+        elif mode == self.RELATIVE:
+            address = int(self.content[self.pointer]) + self.relative_base
+            value = int(self.content[address])
+
+        else:
+            raise ValueError(f'Unknown parameter mode {mode}')
+
+        if DEBUG:
+            print(str(value) + ", ", end='')
+
+        return value, self.pointer + 1
+
+    def set_value(self, value, mode=POSITION):
+        if mode == self.POSITION:
+            address = int(self.content[self.pointer])
+            self.content[address] = value
+
+        elif mode == self.IMMEDIATE:
+            self.content[self.pointer] = value
+
+        if mode == self.RELATIVE:
+            address = int(self.content[self.pointer]) + self.relative_base
+            self.content[address] = value
+
+        return self.pointer + 1
 
 
-def run_op_codes(content):
-    pointer = 0
+class Robot:
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
 
-    while pointer <= len(content) - 1:
-        instruction = content[pointer]
-        op_code = int(instruction[-2:])
-        pointer += 1
+    def __init__(self, brain: IntCodeComputer):
+        self.computer = brain
+        self.panels = [[0 for _ in range(100)] for _ in range(50)]
+        self.x = 0
+        self.y = 50
 
-        if op_code == 99:
-            return
+        self.panels[self.x][self.y] = 1
 
-        if 1 <= op_code <= 2:  # Arithmetic
-            parameter_modes = instruction[:-2].rjust(3, '0')
+        self.painted_tiles = set()
+        self.direction = self.UP
 
-            input_1, pointer = get_value(content, pointer, parameter_modes[-1])
-            input_2, pointer = get_value(content, pointer, parameter_modes[-2])
+    def get_panel(self):
+        return str(self.panels[self.x][self.y])
 
-            if op_code == 1:
-                pointer = set_value(content, pointer, str(input_1 + input_2))
-            elif op_code == 2:
-                pointer = set_value(content, pointer, str(input_1 * input_2))
-
-        elif op_code == 3:
-            pointer = set_value(content, pointer, input("Enter an integer: "))
-
-        elif op_code == 4:
-            parameter_modes = instruction[:-2].rjust(3, '0')
-            value, pointer = get_value(content, pointer, parameter_modes[-1])
-            print(value)
-
-        elif op_code == 5:
-            parameter_modes = instruction[:-2].rjust(2, '0')
-
-            jump_if, pointer = get_value(content, pointer, parameter_modes[-1])
-
-            if jump_if != 0:
-                pointer, _ = get_value(content, pointer, parameter_modes[-2])
+    def turn(self, direction):
+        if direction == 1:
+            if self.direction == self.LEFT:
+                self.direction = self.UP
             else:
-                pointer += 1
-
-        elif op_code == 6:
-            parameter_modes = instruction[:-2].rjust(2, '0')
-
-            jump_if, pointer = get_value(content, pointer, parameter_modes[-1])
-
-            if jump_if == 0:
-                pointer, _ = get_value(content, pointer, parameter_modes[-2])
+                self.direction += 1
+        else:
+            if self.direction == self.UP:
+                self.direction = self.LEFT
             else:
-                pointer += 1
+                self.direction -= 1
 
-        elif 7 <= op_code <= 8:  # Comparative
-            parameter_modes = instruction[:-2].rjust(3, '0')
+    def move_forward(self):
+        if self.direction == self.UP:
+            self.y += 1
+        elif self.direction == self.RIGHT:
+            self.x += 1
+        elif self.direction == self.DOWN:
+            self.y -= 1
+        elif self.direction == self.LEFT:
+            self.x -= 1
 
-            input_1, pointer = get_value(content, pointer, parameter_modes[-1])
-            input_2, pointer = get_value(content, pointer, parameter_modes[-2])
+    def run(self):
+        while True:
+            paint_color = self.computer.run_until_output(self.get_panel)
+            direction = self.computer.run_until_output(self.get_panel)
 
-            if op_code == 7:
-                pointer = set_value(content, pointer, 1 if input_1 < input_2 else 0)
-            elif op_code == 8:
-                pointer = set_value(content, pointer, 1 if input_1 == input_2 else 0)
+            if paint_color is None or direction is None:
+                break
 
-        # print(f"pointer {pointer}", content[pointer], content)
+            self.panels[self.x][self.y] = paint_color
 
+            self.painted_tiles.add((self.x, self.y))
 
-def get_value(content, pointer, mode=ParameterMode.POSITION.value):
-    if mode == ParameterMode.POSITION.value:
-        address = int(content[pointer])
-        value = int(content[address])
-    elif mode == ParameterMode.IMMEDIATE.value:
-        value = int(content[pointer])
-    else:
-        raise ValueError(f'Unknown parameter mode {mode}')
+            self.turn(direction)
 
-    return value, pointer + 1
+            self.move_forward()
 
+        print("Num tiles: ", len(self.painted_tiles))
 
-def set_value(content, pointer, value):
-    address = int(content[pointer])
-    content[address] = value
-
-    return pointer + 1
+    def print(self):
+        for x in range(len(self.panels)):
+            for y in range(len(self.panels[0])):
+                if str(self.panels[x][y]) == '0':
+                    print('⬛', end='')
+                else:
+                    print('⬜', end='')
+            print()
 
 
 def main():
     with open('input.txt', 'r+') as file:
         content = file.read().split(',')
 
-        run_op_codes(content)
+        robot = Robot(IntCodeComputer(content))
+
+        robot.run()
+
+        robot.print()
 
 
 if __name__ == "__main__":
